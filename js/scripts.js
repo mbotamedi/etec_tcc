5113f11dbf5e45c8d0e1fba74dd4fd63452f2d10
ContiRafa <155206195+ContiRafa@users.noreply.github.com>
2025-02-27 17:27:22 -0300

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,52 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+/*---------PROGRAMAÇÃO DO CARROSEL-------------*/
+
+const carousel = document.querySelector('.carrosel');
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const prevSlide = document.querySelector('#prev-slide');
+const nextSlide = document.querySelector('#next-slide');
+
+let currentSlide = 0;
+let intervalId;
+
+function updateCarousel() {
+  carouselItems.forEach((item, index) => {
+    item.classList.remove('active');
+    if (index === currentSlide) {
+      item.classList.add('active');
+    }
+  });
+}
+
+function nextSlideAuto() {
+  currentSlide++;
+  if (currentSlide >= carouselItems.length) {
+    currentSlide = 0;
+  }
+  updateCarousel();
+}
+
+intervalId = setInterval(nextSlideAuto, 7000);
+
+prevSlide.addEventListener('click', () => {
+  clearInterval(intervalId);
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = carouselItems.length - 1;
+  }
+  updateCarousel();
+  intervalId = setInterval(nextSlideAuto, 7000);
+});
+
+nextSlide.addEventListener('click', () => {
+  clearInterval(intervalId);
+  currentSlide++;
+  if (currentSlide >= carouselItems.length) {
+    currentSlide = 0;
+  }
+  updateCarousel();
+  intervalId = setInterval(nextSlideAuto, 7000);
+});
