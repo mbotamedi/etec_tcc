5e4a6dd64d748a51695425602e9703e91aa6a596
mbotamedi <89879002+mbotamedi@users.noreply.github.com>
2025-03-11 17:25:20 -0300

    .

--- HTML/produtos.php
+++ HTML/produtos.php
@@ -23,9 +23,9 @@
             </div>
 
             <div class="items col-5">
-                <a href="index.html">INICIO</a>
-                <a href="HTML/produtos.php">PEÇA AGORA</a>
-                <a href="HTML/unidades.html">UNIDADES</a>
+                <a href="../index.php">INICIO</a>
+                <a href="../html/produtos.php">PEÇA AGORA</a>
+                <a href="../html/unidades.html">UNIDADES</a>
 
             </div>
 
