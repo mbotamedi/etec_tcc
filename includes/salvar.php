5945184f34602c14e87e14fc52af8a4473370b8a
ContiRafa <155206195+ContiRafa@users.noreply.github.com>
2025-03-23 12:39:08 -0300

    ..

--- includes/salvar.php
+++ includes/salvar.php
@@ -15,17 +15,18 @@
 
     if ($senha === $confirmar) {
         $senhaCad =  $senha;
-        $gravar = mysqli_query($conexao,"INSERT INTO tb_cliente  VALUES(0,'$nome','$cpf', '$email', '$celular','$senhaCad')");
+        $gravar = mysqli_query($conexao,"INSERT INTO tb_clientes  VALUES(0,'$nome','$cpf', '$email', '$celular','$senhaCad')");
 
         if ($gravar){
             echo "Dados salvos com sucesso";
+            header("Location: ../php/login.php");
         }else{
             echo "Erro ao tentar cadastrar cliente";
         }
     } else {        
         // Se as senhas não coincidirem, armazena a mensagem de erro na sessão e redireciona
         $_SESSION['erro_senha'] = "Senha diferente, favor digitar a mesma senha";
-        header("Location: ../html/cadastro.php"); // Redireciona para o cadastro
+        header("Location: ../php/cadastro.php"); // Redireciona para o cadastro
         exit(); // Certifica-se de que o script seja interrompido após o redirecionamento
     }
 
