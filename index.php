4afbff9fb51e8b4fee5ecee476942e45d88d486e
ContiRafa <155206195+ContiRafa@users.noreply.github.com>
2025-03-31 19:40:00 -0300

    .

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ session_start()
     </div>
 
     <!-- Modal DEPOIS-->
-    <div id="meuModal" class="modal-container">
+    <div id="meuModal" class="modal-container02">
         <!--CONTAINER DE FORA---->
         <div class="modal-content col-3">
             <!--CONTAINER DO NOME---->
