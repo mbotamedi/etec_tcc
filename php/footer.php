3d501b81b5d19209eeb6884b642da763e8a7c284
mbotamedi <botamedis@gmail.com>
2025-04-23 21:36:27 -0300

    .

--- php/footer.php
+++
@@ -1,9 +0,0 @@
-
- 
- <!-- Footer-->
- <footer class="py-5 bg-dark">
-        <div>
-            <p class="m-0 text-center text-white">Copyright &copy; Cantina Três Irmãos</p>
-        </div>
-    </footer>
-    <!-- Footer End-->
--- php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-// Destrói a sessão
-session_destroy();
-header('Location: ../index.php');
-exit;
