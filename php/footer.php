3c3402c968ebcecda0393541f0dad2471128bb5f
ContiRafa <155206195+ContiRafa@users.noreply.github.com>
2025-05-04 17:40:01 -0300

    .

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,11 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+$is_finalizar_pedido = ($current_page == 'finalizar_pedido.php' || $current_page == 'confirmacao_pedido.php');
+?>
+
+
 <!-- Footer-->
+<<<<<<< Updated upstream
 <?php
 $current_page = basename($_SERVER['PHP_SELF']);
 $is_finalizar_pedido = ($current_page == 'cadastro_endereco.php' || $current_page == 'finalizar_pedido.php' || $current_page == 'pedidos_cliente.php' || $current_page == 'confirmacao_pedido.php');
@@ -45,10 +52,33 @@ $is_finalizar_pedido = ($current_page == 'cadastro_endereco.php' || $current_pag
                     </li>
                 </ul>
             </div>
+=======
+ 
+
+
+<head>
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+
+<?php if (!$is_finalizar_pedido): ?>
+<footer class="py-5 bg-dark">
+    <div class="info-footer">
+        <div class="information-divs">
+            <h2 style="color: white;">Sobre nós</h2>
+            <ul class="ul-content">
+                <li>
+                    <a href="unidades.php">Nossa história</a>
+                </li>
+                <li>
+                    <a href="termos_Uso.php">Termos de uso</a>
+                </li>
+            </ul>
+>>>>>>> Stashed changes
         </div>
         <div>
             <p class="m-0 text-center text-white">Copyright &copy; Cantina Três Irmãos</p>
         </div>
+<<<<<<< Updated upstream
     <?php else: ?>
 
         <head>
@@ -99,3 +129,62 @@ $is_finalizar_pedido = ($current_page == 'cadastro_endereco.php' || $current_pag
 
         </footer>
         <!-- Footer End-->
+=======
+        <div class="information-divs">
+            <h2 style="color: white;">Atendimento</h2>
+            <ul class="ul-content">
+                
+                <li>
+                    <a href="faleConosco.php">Fale conosco</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
+    
+
+    <?php else: ?>
+<footer class="py-5 bg-dark">
+    <div class="info-footer">
+        <div class="information-divs">
+            <h2 style="color: white;">Sobre nós</h2>
+            <ul class="ul-content">
+                <li>
+                    <a href="../php/unidades.php">Nossa história</a>
+                </li>
+                <li>
+                    <a href="../php/termos_Uso.php">Termos de uso</a>
+                </li>
+            </ul>
+        </div>
+        <div class="information-divs">
+            <h2 style="color: white;">Dúvidas</h2>
+            <ul class="ul-content">
+                <li>
+                    <a href="../php/compra_segura.php">É seguro comprar?</a>
+                </li>
+                <li>
+                    <a href="../php/info_pedidos.php">Como funciona os Pedidos</a>
+                </li>
+                <li>
+                    <a href="../php/reembolso.php">Política de Reembolso</a>
+                </li>
+            </ul>
+        </div>
+        <div class="information-divs">
+            <h2 style="color: white;">Atendimento</h2>
+            <ul class="ul-content">
+                
+                <li>
+                    <a href="../php/faleConosco.php">Fale conosco</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div>
+        <p class="m-0 text-center text-white">Copyright &copy; Cantina Três Irmãos</p>
+    </div>
+    <?php endif; ?>
+</footer>
+<!-- Footer End-->
+>>>>>>> Stashed changes
